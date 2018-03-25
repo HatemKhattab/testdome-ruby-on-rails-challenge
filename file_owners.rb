ddcc7b6f9cbb273119fa5fe95f2bf0149f3dd0d6
Hatem Khattab <hattemkhattab@gmail.com>
2018-03-25 16:07:40 +0200

    add file_owners

---
+++ file_owners.rb
@@ -0,0 +1,23 @@
+module FileOwners
+  
+  def self.group_by_owners(files)
+    result = {}
+    files.each do |key, value|
+      if result.has_key?(value)
+        result[value] << key
+      else
+        result["#{value}"] = [key]
+      end
+    end
+    result
+  end
+end
+  
+files = {
+  'Input.txt' => 'Randy',
+  'Code.py' => 'Stan',
+  'Output.txt' => 'Randy'
+}
+
+puts FileOwners.group_by_owners(files)
+
