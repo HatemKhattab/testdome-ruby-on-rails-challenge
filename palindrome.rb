c5622cb8245709341efc48cc4fb7850c22e0b25a
Hatem Khattab <hattemkhattab@gmail.com>
2018-04-03 20:28:30 +0200

    refactor the function with with one line

--- palindrome.rb
+++ palindrome.rb
@@ -1,18 +1,6 @@
 module Palindrome
   def self.is_palindrome(word)
-    string = word.downcase
-    string_length = word.length
-    array_1 = []
-    array_2 = []
-
-    for i in 0..string_length-1 do
-      array_1 << string[i]
-    end
-
-    (string_length-1).downto(0) do |i|
-      array_2 << string[i]
-    end
-    array_1 == array_2
+    word.downcase == word.downcase.reverse
   end
 end
 
